fc9107eb8da36ad4d3611735eb5049e1c225f4b3
Kevin Baxter <baxterhome@BaxterDevPro.local>
2020-02-01 02:03:43 -0500

    first commit/working app

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OddIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 1;
+            
+            for(int i = 1; i <= 7; i++){
+
+              if(i % 2 != 0){
+                number *= i;
+               
+              }
+            }
+
+            Console.WriteLine(number);
+        }
+    }
+}
